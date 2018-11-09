26795158c8a4cdb3451ef902a978a83cb3ff1fc3
Ethan <ethan@webcomm.com.tw>
2018-11-09 10:34:25 +0800

    winner

--- src/index.js
+++ src/index.js
@@ -2,66 +2,110 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-//Functional Component(單純render UI stateless components)
-const TodoList = (props) => (
-    <ul>
-        {
-            props.items.map((item) => (
-                <li key={item.id}>{item.text}</li>
-            ))
+//Function Components
+function Square(props){
+    return (
+        <button
+            className="square"
+            onClick={() => props.onClick()}
+        >
+            {props.value}
+        </button>
+    )
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+    for (let i = 0; i < lines.length; i++){
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
         }
-    </ul>
-)
-//Class Component(可進行較複雜的操作和元件生命週期的控制)
-class TodoApp extends React.Component {
+    }
+    return null;
+}
+
+class Board extends React.Component {
     constructor(props) {
-      super(props);
-      this.onChange = this.onChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.state = {
-          items: [],
-          text: '',
-      }
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        }
     }
-    onChange(e) {
-        this.setState({text: e.target.value});
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        // if someone has won the game or if a Square is already filled
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext? 'X' : 'O';
+        console.log(squares);
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
-    handleSubmit(e) {
-        e.preventDefault();
-        const nextItems = this.state.items.concat( [{text: this.state.text, id: Date.now()}] );
-        const nextText = '';
-        this.setState({items: nextItems, text: nextText});
+
+    renderSqaure(i) {
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick={ () => this.handleClick(i)}
+            />
+        );
     }
     render() {
+
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div>
-                <h3>TODO</h3>
-                <TodoList items={this.state.items} />
-                <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.onChange} value={this.state.text} />
-                    <button>{'Add #' + (this.state.items.length + 1)}</button>
-                </form>
+                <div className="status">{status}</div>
+                <div className="border-row">
+                    {this.renderSqaure(0)}{this.renderSqaure(1)}{this.renderSqaure(2)}
+                </div>
+                <div className="border-row">
+                    {this.renderSqaure(3)}{this.renderSqaure(4)}{this.renderSqaure(5)}
+                </div>
+                <div className="border-row">
+                    {this.renderSqaure(6)}{this.renderSqaure(7)}{this.renderSqaure(8)}
+                </div>
             </div>
-        )
-    }
-    componentWillMount() {
-        console.log('componentWillMount');
-    }
-    componentDidMount() {
-        console.log('componentDidMount');
-    }
-    componentWillReceiveProps() {
-        console.log('componentWillReceiveProps');
-    }
-    componentWillUpdate() {
-        console.log('componentWillUpdate');
-    }
-    componentDidUpdate() {
-        console.log('componentDidUpdate');
-    }
-    componentWillUnmount() {
-        console.log('componentWillUnmount');
+        );
     }
 }
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-boarder">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div></div>
+                    <ol></ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Game />, document.getElementById('root'));
